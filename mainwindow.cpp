5c12c1fb0d2201812f41d9b9431da71d6e0fecf1
lvefjord <theivefjord@gmail.com>
2024-12-05 18:54:39 +0100

    first commit most base stuff done

---
+++ mainwindow.cpp
@@ -0,0 +1,309 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "QMessageBox"
+#include "QRegularExpression"
+#include "QScrollBar"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    connect(ui->actionOpen, &QAction::triggered, this, &MainWindow::openFile);
+    connect(ui->actionSave, &QAction::triggered, this, &MainWindow::saveFile);
+    connect(ui->actionSave_As, &QAction::triggered, this, &MainWindow::saveFileAs);
+    connect(ui->actionClose, &QAction::triggered, this, &MainWindow::closeFile);
+    connect(ui->actionReload_File, &QAction::triggered, this, &MainWindow::reloadFile);
+    currentFile = "";
+    setWindowTitle(tr("Untitled"));
+    connect(ui->nameField, &QLineEdit::textEdited, this, &MainWindow::on_FieldEdited);
+    connect(ui->dnaField, &QLineEdit::textEdited, this, &MainWindow::on_FieldEdited);
+    connect(ui->religionField, &QLineEdit::textEdited, this, &MainWindow::on_FieldEdited);
+    connect(ui->cultureField, &QLineEdit::textEdited, this, &MainWindow::on_FieldEdited);
+    connect(ui->dynField, &QLineEdit::textEdited, this, &MainWindow::on_FieldEdited);
+    connect(ui->bdayField, &QLineEdit::textEdited, this, &MainWindow::on_FieldEdited);
+    connect(ui->deathdayField, &QLineEdit::textEdited, this, &MainWindow::on_FieldEdited);
+    characters.append(QMap<QString, QString>());
+    // setup trait tabs
+    QSizePolicy sp_retain = ui->traitTabs->sizePolicy();
+    sp_retain.setRetainSizeWhenHidden(true);
+    ui->traitTabs->setSizePolicy(sp_retain);
+    ui->traitTabs->setVisible(false);
+}
+
+void MainWindow::openFile()
+{
+    QString fileName = QFileDialog::getOpenFileName(this, tr("Open File"), "", tr("Text Files (*.txt);;All Files (*)"));
+    if (!fileName.isEmpty())
+    {
+        QFile file(fileName);
+        if (file.open(QIODevice::ReadOnly | QIODevice::Text))
+        {
+            QTextStream in(&file);
+            ui->openFileEdit->setPlainText(in.readAll());
+            file.close();
+            currentFile = fileName;
+            setWindowTitle(fileName);
+        }
+    }
+}
+void MainWindow::saveFileAs()
+{
+    QString fileName = QFileDialog::getSaveFileName(this, tr("Save File"), "", tr("Text Files (*.txt);;All Files (*)"));
+    if (!fileName.isEmpty())
+    {
+        QFile file(fileName);
+        if (file.open(QIODevice::WriteOnly | QIODevice::Text))
+        {
+            QTextStream out(&file);
+            out << ui->openFileEdit->toPlainText();
+            file.close();
+        }
+    }
+}
+void MainWindow::saveFile()
+{
+    if (currentFile.isEmpty())
+    {
+        saveFileAs();
+    }
+    else
+    {
+        QFile file(currentFile);
+        if (file.open(QIODevice::WriteOnly | QIODevice::Text))
+        {
+            QTextStream out(&file);
+            out << ui->openFileEdit->toPlainText();
+            file.close();
+        }
+    }
+}
+void MainWindow::closeFile()
+{
+    if (ui->openFileEdit->document()->isModified())
+    {
+        QMessageBox::StandardButton reply = QMessageBox::question(
+            this,
+            tr("Unsaved Changes"),
+            tr("You have unsaved changes. Do you want to save them before closing?"),
+            QMessageBox::Yes | QMessageBox::No | QMessageBox::Cancel
+        );
+
+        if (reply == QMessageBox::Yes)
+        {
+            saveFile();
+        }
+        else if (reply == QMessageBox::Cancel)
+        {
+            return;
+        }
+    }
+
+    ui->openFileEdit->clear();
+    currentFile = "";
+    ui->openFileEdit->document()->setModified(false);
+    setWindowTitle(tr("Untitled"));
+}
+void MainWindow::reloadFile()
+{
+    if (ui->openFileEdit->document()->isModified())
+    {
+        QMessageBox::StandardButton reply = QMessageBox::question(
+            this,
+            tr("Unsaved Changes"),
+            tr("You have unsaved changes. Do you want to save them before closing?"),
+            QMessageBox::Yes | QMessageBox::No | QMessageBox::Cancel
+            );
+
+        if (reply == QMessageBox::Yes)
+        {
+            saveFile();
+        }
+        else if (reply == QMessageBox::Cancel)
+        {
+            return;
+        }
+    }
+    if (!currentFile.isEmpty())
+    {
+        QFile file(currentFile);
+        if (file.open(QIODevice::ReadOnly | QIODevice::Text))
+        {
+            QTextStream in(&file);
+            ui->openFileEdit->setPlainText(in.readAll());
+            file.close();
+            ui->openFileEdit->document()->setModified(false);
+            statusBar()->showMessage(tr("File reloaded successfully"), 3000);
+            // make the program blink or something here for feedback otherwise you'll feel unsure
+        }
+    }
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+QString MainWindow::formatCharacter(const QMap<QString, QString> &character, const QStringList &fieldOrder, int characterIndex)
+{
+    QString formattedText;
+
+    if (character.contains("dynasty"))
+    {
+        QString dynasty = character["dynasty"];
+        formattedText += QString("%1_%2 = {\n").arg(dynasty).arg(characterIndex);
+    }
+
+    for (const QString &orderedKey : fieldOrder)
+    {
+        if (orderedKey == "birth" && character.contains(orderedKey) && !character[orderedKey].isEmpty())
+        {
+            formattedText += QString("    %1 = {\n        birth = yes\n    }\n").arg(character[orderedKey]);
+        }
+        else if (orderedKey == "death" && character.contains(orderedKey) && !character[orderedKey].isEmpty())
+        {
+            formattedText += QString("    %1 = { death = yes }\n").arg(character[orderedKey]);
+        }
+        else if (character.contains(orderedKey) && !character[orderedKey].isEmpty())
+        {
+            formattedText += QString("    %1 = %2\n").arg(orderedKey, character[orderedKey]);
+        }
+    }
+    formattedText += "}\n\n";
+    return formattedText;
+}
+
+void MainWindow::on_FieldEdited(const QString &text)
+{
+    QLineEdit *lineEdit = qobject_cast<QLineEdit *>(sender());
+    QString key;
+
+    if (lineEdit == ui->nameField)
+        key = "name";
+    else if (lineEdit == ui->dnaField)
+        key = "dna";
+    else if (lineEdit == ui->religionField)
+        key = "religion";
+    else if (lineEdit == ui->cultureField)
+        key = "culture";
+    else if (lineEdit == ui->dynField)
+        key = "dynasty";
+    else if (lineEdit == ui->bdayField)
+        key = "birth";
+    else if (lineEdit == ui->deathdayField)
+        key = "death";
+
+    if (!key.isEmpty() && !characters.isEmpty())
+    {
+        // scrollbar stuff
+        QScrollBar *scrollBar = ui->openFileEdit_newchars->verticalScrollBar();
+        int scrollPosition = scrollBar->value();
+
+        characters[characters.size() - 1][key] = text;
+
+        QString updatedText;
+
+        for (int i = 0; i < characters.size(); ++i)
+        {
+            updatedText += formatCharacter(characters[i], fieldOrder, i + 1);
+        }
+
+        ui->openFileEdit_newchars->setPlainText(updatedText);
+
+        scrollBar->setValue(scrollPosition);
+    }
+}
+
+void MainWindow::on_addCharacter_clicked()
+{
+    QMap<QString, QString> currentCharacter;
+    currentCharacter["name"] = ui->nameField->text();
+    currentCharacter["dna"] = ui->dnaField->text();
+    currentCharacter["father"] = "";
+    currentCharacter["mother"] = "";
+    currentCharacter["religion"] = ui->religionField->text();
+    currentCharacter["culture"] = ui->cultureField->text();
+    currentCharacter["dynasty"] = ui->dynField->text();
+    currentCharacter["birth"] = ui->bdayField->text();
+    currentCharacter["death"] = ui->deathdayField->text();
+
+    characters.append(currentCharacter);
+
+    //ui->nameField->clear();
+    //ui->dnaField->clear();
+    //ui->religionField->clear();
+    //ui->cultureField->clear();
+    //ui->dynField->clear();
+    //ui->bdayField->clear();
+    //ui->deathdayField->clear();
+    ui->female_checkBox->setChecked(false);
+    ui->rtraits_checkBox->setChecked(false);
+
+    QString updatedText;
+
+    for (int i = 0; i < characters.size(); ++i)
+    {
+        updatedText += formatCharacter(characters[i], fieldOrder, i + 1);
+    }
+
+    ui->openFileEdit_newchars->setPlainText(updatedText);
+    ui->openFileEdit_newchars->verticalScrollBar()->setValue(ui->openFileEdit_newchars->verticalScrollBar()->maximum());
+}
+
+void MainWindow::on_female_checkBox_stateChanged(int state)
+{
+    if (characters.isEmpty())
+        return;
+
+    QMap<QString, QString> &activeCharacter = characters[characters.size() - 1];
+
+    if (state == Qt::Checked)
+    {
+        activeCharacter["female"] = "yes";
+    }
+    else
+    {
+        activeCharacter.remove("female");
+    }
+
+    QString updatedText;
+
+    for (int i = 0; i < characters.size(); ++i)
+    {
+        updatedText += formatCharacter(characters[i], fieldOrder, i + 1);
+    }
+
+    ui->openFileEdit_newchars->setPlainText(updatedText);
+    ui->openFileEdit_newchars->verticalScrollBar()->setValue(ui->openFileEdit_newchars->verticalScrollBar()->maximum());
+}
+
+
+void MainWindow::on_rtraits_checkBox_stateChanged(int state)
+{
+    if (characters.isEmpty())
+        return;
+
+    QMap<QString, QString> &activeCharacter = characters[characters.size() - 1];
+
+    if (state == Qt::Checked)
+    {
+        activeCharacter["disallow_random_traits"] = "yes";
+        ui->traitTabs->setVisible(true);
+    }
+    else
+    {
+        activeCharacter.remove("disallow_random_traits");
+        ui->traitTabs->setVisible(false);
+    }
+
+    QString updatedText;
+
+    for (int i = 0; i < characters.size(); ++i)
+    {
+        updatedText += formatCharacter(characters[i], fieldOrder, i + 1);
+    }
+
+    ui->openFileEdit_newchars->setPlainText(updatedText);
+    ui->openFileEdit_newchars->verticalScrollBar()->setValue(ui->openFileEdit_newchars->verticalScrollBar()->maximum());
+}
+
---
+++ mainwindow.h
@@ -0,0 +1,47 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QFileDialog>
+#include <QTextStream>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+    QString currentFile;
+    QMap<QString, QString> fieldValues;
+    QList<QMap<QString, QString>> characters;
+    QString formatCharacter(const QMap<QString, QString> &character, const QStringList &fieldOrder, int characterIndex);
+    const QStringList fieldOrder = {
+        "name", "dynasty", "dna", "female", "culture", "religion", "disallow_random_traits", "birth", "death"
+    };
+
+
+private slots:
+    void openFile();
+    void saveFileAs();
+    void saveFile();
+    void closeFile();
+    void reloadFile();
+    // remember to make a death reason selection input after the death date thingy
+    //void on_nameField_textEdited(const QString &text);
+    void on_FieldEdited(const QString &text);
+    void on_addCharacter_clicked();
+    void on_female_checkBox_stateChanged(int arg1);
+    void on_rtraits_checkBox_stateChanged(int arg1);
+};
+
+#endif // MAINWINDOW_H
